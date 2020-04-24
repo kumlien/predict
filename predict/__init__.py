44b9f5743cb257cd75630c41bd185a05324eac90
Svante Kumlien <svante.kumlien@gmail.com>
2020-04-24 17:50:00 +0200

    add func to reset password via email

--- predict/__init__.py
+++ predict/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a8916b4c43fa5d3629fe108c89c48992'
@@ -13,5 +15,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' #function name of the route
 login_manager.login_message_category = 'info' #A bootstrap class
+
+app.config['MAIL_SERVER'] = os.environ.get('EMAIL_SERVER') # like 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = '587'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from predict import routes
--- predict/routes.py
+++ predict/routes.py
@@ -2,15 +2,19 @@ import secrets
 import os
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from predict import app, db, bcrypt
-from predict.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from predict import app, db, bcrypt, mail
+from predict.forms import (RegistrationForm, LoginForm, UpdateAccountForm, 
+PostForm, RequestResetForm, ResetPasswordForm)
 from predict.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
+from flask_mail import Message
 
 
 @app.route("/")
+@app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template('index.html', posts=posts)
 
 
@@ -133,3 +137,55 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted', 'success')
     return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).\
+        order_by(Post.date_posted.desc())\
+        .paginate(per_page=5, page=page)
+    return render_template('user_posts.html', posts=posts, user=user)
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password reset request', 
+                    sender='noreply@demo.com', 
+                    recipients=[user.email])
+    msg.body = f'''To reset you password visit the following link:
+    {url_for('reset_token', token=token, _external=True)}
+
+    If you did not make this request then simply ignore this email.
+    '''
+    mail.send(msg)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', form=form, title='Reset Password')
+
+@app.route("/reset_password/<string:token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_pwd = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_pwd
+        db.session.commit()
+        flash('Your password has been updated', 'success') # success is a bootstrap class
+        return redirect(url_for('home'))
+    return render_template('reset_token.html', form=form, title='Reset Password')
